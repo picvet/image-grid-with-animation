b28c6bc6be07eca607783c784e89e59459d10091
Picvet <percyzulu2@outlook.com>
2022-12-18 15:34:16 +0200

    Responsive images

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'saira': ['Saira Extra Condensed'],
+    }
+  },
+  plugins: [],
+}
